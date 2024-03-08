b4f31a7e93b1666df1fe52470c1205ceb7f5d617
ved <vedbansal31@gmail.com>
2024-03-08 15:30:20 +0530

    added fonts in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'font-serif': ["ui-serif", "Georgia", "Cambria", "Times New Roman", "Times", "serif"],
+    },
   },
   plugins: [],
 }
